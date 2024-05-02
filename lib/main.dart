28d40611f4f61fbaa5a209ffdac7096d9fce7cc1
Mishal Ahamed <iyaadluq@gmail.com>
2024-05-02 23:27:55 +0530

    Fixed Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import './splash.dart';
 // Removed unused import
 // import './dashboard.dart'; // import your DashboardScreen
 
